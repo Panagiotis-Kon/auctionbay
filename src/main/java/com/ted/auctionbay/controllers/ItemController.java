079001ad038b586f04ea2e68e530fb74925fb6cb
Panagiotis Kon <kls.panagiotis@gmail.com>
2016-07-23 19:25:04 +0300

    ItemServices expand

--- src/main/java/com/ted/auctionbay/controllers/ItemController.java
+++ src/main/java/com/ted/auctionbay/controllers/ItemController.java
@@ -1,12 +1,18 @@
 package com.ted.auctionbay.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ted.auctionbay.services.ItemServices;
 
+@Controller
+@RequestMapping(value={"/item", "/user/{username}/item"})
 public class ItemController {
 
 	@Autowired
 	ItemServices itemServices;
+	
+	
 	
 }
