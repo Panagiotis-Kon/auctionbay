862e29de6d13b29f9a07feffc0b50d218063fef9
alextsak <alextsak93@gmail.com>
2016-08-10 12:35:50 +0300

    implementing messages

--- src/main/webapp/resources/js/custom/user/mailbox.js
+++ src/main/webapp/resources/js/custom/user/mailbox.js
@@ -13,11 +13,34 @@ $(document).ready(function (){
 
 function initListeners() {
 	
+	$("#recipient").click(function(event){
+		event.preventDefault();
+		
+	});
+	
 	$("#compose-button").click(function(e){
 		e.preventDefault();
 		$("#main-content-area").css("display","none");
 		$("#compose-area").css("display","block");
+		$("#compose-button").css("display","none");
+		$("#back-to-read").css("display","block");
 		
 	});
+	
+	$("#back-to-read").click(function(e){
+		e.preventDefault();
+		$("#compose-area").css("display","none");
+		$("#main-content-area").css("display","block");
+		$("#back-to-read").css("display","none");
+		$("#compose-button").css("display","block");
+		
+		
+	});
+	
+}
+
+
+function getRecipients() {
+	
 	
 }
